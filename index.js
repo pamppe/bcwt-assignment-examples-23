9c1afd2f5efb9fcb858c7247f7e3f7efc8b58261
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-03-15 09:28:14 +0200

    kisuja

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 app.get("/catinfo", (req, res) => {
     const cat = {
