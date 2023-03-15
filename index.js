6cb648981a08e1fa03a584edcc185659623816fa
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-03-15 11:30:17 +0200

    pug

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = 3000;
 
 app.use(express.static('public'))
 
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    const username = 'Aku Ankka';
+    const description = 'Ankkalinna asukas.';
+    const content = {title: username, description};
+    res.render('index', content);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
