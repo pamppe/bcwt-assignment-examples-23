1d2e9b380c78ef2a02d4d7d3237bb98596275351
Emil Lehtonen <emil.lehikoinen@outlook.com>
2023-03-20 11:29:01 +0200

    cat controller on process

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,19 @@
 'use strict';
 // catController
+const catModel = require('../models/catModel');
+
+const cats = catModel.cats;
+const getCatList = (req, res) => {
+    res.json(cats);
+};
+const getCat = (req, res) => {
+    //console.log(req.params);
+    const id = req.params.catId;
+    //TODO: filter matching cat based on id
+    // TODO: response 404 if id not found in array (res.status(404))
+    const cat = cats[0];
+    res.json(cat);
+}
+
+const catController = {getCatList, getCat};
+module.exports = catController;
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,21 @@
 'use strict';
 // catRoute
+const express = require('express');
+const router = express.Router();
+const catController = require('../controllers/catController');
+
+router.get('/', catController.getCatList);
+router.get('/:catId', catController.getCat);
+
+//TODO: move functions below to controller
+router.post('/', (req, res) => {
+    res.send('With this endpoint you can add cats.');
+});
+router.put('/', (req, res) => {
+    res.send('With this endpoint you can modify cats.');
+});
+router.delete('/', (req, res) => {
+    res.send('With this endpoint you can delete cats.');
+});
+
+module.exports = router;
